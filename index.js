f41e3b5258ed36775feb1b2d38fddc006856ddce
negitoro.2007 <negitoro.2007@gmail.com>
2024-10-31 06:45:52 +0900

    ログの日付を削除

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require("node:http");
 const pug = require("pug");
 const server = http
   .createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    console.info(` Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       "Content-Type": "text/html; charset=utf-8",
     });
@@ -65,7 +64,7 @@ const server = http
             const body = `${answer.get("name")}さんは${answer.get(
               "favorite"
             )}に投票しました`;
-            console.info(`[${now}] ${body}`);
+            console.info(`${body}`);
             res.write(
               `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
             );
